9c3d7bfe16f87bf851e8a14ad3ea86bdd1ff1995
davyken <davykennang552@gmail.com>
2026-03-02 15:32:07 +0100

    updated dockerfile

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -10,4 +10,28 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// Add request interceptor
+api.interceptors.request.use(
+  (config) => {
+    console.log("API Request:", config.method?.toUpperCase(), config.url);
+    return config;
+  },
+  (error) => {
+    console.error("API Request Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor
+api.interceptors.response.use(
+  (response) => {
+    console.log("API Response:", response.status, response.config.url);
+    return response;
+  },
+  (error) => {
+    console.error("API Response Error:", error.response?.status, error.message);
+    return Promise.reject(error);
+  }
+);
+
 export default api;
